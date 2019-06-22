43961f86cbb0fea74b8ad847d21d5d4535f23fd5
jjy <galid1@naver.com>
2019-06-23 00:14:26 +0900

    develop - UploadTicketException 이름 TicketException으로 변경

--- src/main/java/com/photosend/photosendserver01/user/domain/exception/UploadTicketException.java
+++
@@ -1,7 +0,0 @@
-package com.photosend.photosendserver01.user.domain.exception;
-
-public class UploadTicketException extends RuntimeException {
-    public UploadTicketException(String message) {
-        super(message);
-    }
-}
