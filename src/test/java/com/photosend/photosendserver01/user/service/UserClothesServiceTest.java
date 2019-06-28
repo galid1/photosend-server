270b8229ca465a68aeb8fc0be0ba13651a3c8ed7
전준엽 <galid9619@gmail.com>
2019-06-28 16:11:33 +0900

    develop - UserClothesService uploadClothesImage Test 코드 작성

--- src/test/java/com/photosend/photosendserver01/user/service/UserClothesServiceTest.java
+++ src/test/java/com/photosend/photosendserver01/user/service/UserClothesServiceTest.java
@@ -1,16 +1,17 @@
 package com.photosend.photosendserver01.user.service;
 
-import com.photosend.photosendserver01.user.domain.*;
-import com.photosend.photosendserver01.user.presentation.ClothesImageUrl;
-import org.junit.Before;
+import com.photosend.photosendserver01.user.domain.ClothesEntity;
+import com.photosend.photosendserver01.user.domain.UserEntity;
+import com.photosend.photosendserver01.user.domain.UserInformation;
+import com.photosend.photosendserver01.user.domain.UserRepository;
+import com.photosend.photosendserver01.user.infra.file.ImageType;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.web.multipart.MultipartFile;
 
 import static org.junit.Assert.*;
 
@@ -27,24 +28,25 @@ public class UserClothesServiceTest {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    UploadFileUtil uploadFileUtil;
+
+    // notEquals를 통해 랜덤으로 생성된 이름으로 업로드 되는지 테스트를 함(과 동시에 통과시 자연스럽게 업로드 테스트를 한것과 동일시 됨)
     @Test
     public void uploadClothesImageTest() {
         // given
         Long userId = registerService.registerUser(UserInformation.builder().name("jjy").age(12).build());
-
-        List<ClothesImageUrl> clothesImageUrlList = new ArrayList<>();
-        clothesImageUrlList.add(ClothesImageUrl.builder().clothesImageUrl("a").build());
-        clothesImageUrlList.add(ClothesImageUrl.builder().clothesImageUrl("b").build());
-        clothesImageUrlList.add(ClothesImageUrl.builder().clothesImageUrl("c").build());
+        String fileName = "file1";
+        MockMultipartFile file1 = new MockMultipartFile(fileName, "test.txt", null, "test data.".getBytes());
 
         // when
-        userClothesService.uploadClothesImages(userId, clothesImageUrlList);
+        userClothesService.uploadClothesImages(userId, new MultipartFile[]{file1});
 
         // then
-        List<ClothesEntity> clothesList = userRepository.findById(userId).get().getClothesList();
-        assertEquals(clothesList.get(0).getClothesImagePath(), "a");
-        assertEquals(clothesList.get(1).getClothesImagePath(), "b");
-        assertEquals(clothesList.get(2).getClothesImagePath(), "c");
+        UserEntity userEntity = userRepository.findById(userId).get();
+        ClothesEntity clothesEntity = userEntity.getClothesList().get(0);
+        String uploadPath = uploadFileUtil.makeFileUploadPath(userId, fileName, ImageType.CLOTHES);
+        assertNotEquals(uploadPath, clothesEntity.getClothesImagePath());
     }
 
 
