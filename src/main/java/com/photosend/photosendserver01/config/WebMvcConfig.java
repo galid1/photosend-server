380cdd67f71c9d0f441b5188ae49195a44682d62
전준엽 <galid9619@gmail.com>
2019-07-25 10:44:03 +0900

    develop - 사용자가 orders/ 이하의 API 요청시 토큰인증을 하도록 Interceptor에 경로 추가

--- src/main/java/com/photosend/photosendserver01/config/WebMvcConfig.java
+++ src/main/java/com/photosend/photosendserver01/config/WebMvcConfig.java
@@ -11,7 +11,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(checkTokenInterceptor()).addPathPatterns("/users/{users-id}/**");
+        registry.addInterceptor(checkTokenInterceptor())
+                    .addPathPatterns("/users/{users-id}/**")
+                    .addPathPatterns("/orders/**");
     }
 
     @Override
