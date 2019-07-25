a41ada6c5bc37bbbc993f6b2f4df8229aab79098
전준엽 <galid9619@gmail.com>
2019-07-25 10:09:23 +0900

    develop - AdminProductController API의 url을 /admin/** 으로 변경 - admin domain 생성

--- src/main/java/com/photosend/photosendserver01/domains/admin/presentation/AdminProductController.java
+++ src/main/java/com/photosend/photosendserver01/domains/admin/presentation/AdminProductController.java
@@ -1,21 +1,19 @@
-package com.photosend.photosendserver01.domains.user.presentation;
+package com.photosend.photosendserver01.domains.admin.presentation;
 
 import com.photosend.photosendserver01.domains.user.presentation.request_reponse.FoundProductInformation;
 import com.photosend.photosendserver01.domains.user.service.AdminProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/admin")
 public class AdminProductController {
     @Autowired
     private AdminProductService adminclothesService;
 
-    @PostMapping("/users/{usersId}/product/information")
+    @PostMapping("/{usersId}/product/information")
     public List<Long> putFoundClothesInformation(@PathVariable("usersId")String uid, @RequestBody List<FoundProductInformation> foundProductInformationList) {
         return adminclothesService.putFoundProductInformation(uid, foundProductInformationList);
     }
