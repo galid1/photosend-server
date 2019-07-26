a3ecd8c8d1948abf1e3f83135777a9db0c07ca59
전준엽 <galid9619@gmail.com>
2019-07-26 16:56:59 +0900

    develop - OrderState 상태 이름 변경

--- src/main/java/com/photosend/photosendserver01/domains/order/domain/OrderState.java
+++ src/main/java/com/photosend/photosendserver01/domains/order/domain/OrderState.java
@@ -2,7 +2,7 @@ package com.photosend.photosendserver01.domains.order.domain;
 
 public enum OrderState {
      // 결제 완료                             // 배송중                                // 배송완료                         //주문 취소
-    PAYMENT_COMPLETE("paymeny complete"),  SHIP_IN_PROGRESS("ship in progress"), SHIP_COMPLETE("ship complete"), ORDER_CANCELD("order cancled");
+    PAYMENT_COMPLETED("paymeny complete"),  SHIPPING_IN_PROGRESS("ship in progress"), SHIPMENT_COMPLETED("ship complete"), ORDER_CANCELD("order cancled");
 
     private String orderState;
 
