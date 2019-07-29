7b28c4c4f2c4501a8b8e7b967fd2e3f79ab070d8
전준엽 <galid9619@gmail.com>
2019-07-29 15:14:44 +0900

    develop - AdminChangeOrderStateService의 changeOrderStateTo() 메소드 리팩토링

--- src/main/java/com/photosend/photosendserver01/domains/admin/service/AdminChangeOrderStateService.java
+++ src/main/java/com/photosend/photosendserver01/domains/admin/service/AdminChangeOrderStateService.java
@@ -24,16 +24,10 @@ public class AdminChangeOrderStateService {
                 && orderState != OrderState.SHIPMENT_COMPLETED)
             throw new IllegalArgumentException("변경을 원하는 상태가 결제완료 또는 배송완료여야 합니다.");
 
-        Long returnOrdersId = null;
         OrderEntity orderEntity = orderRepository.findById(ordersId)
                 .orElseThrow(() -> new UserNotFoundException("사용자가 존재하지 않습니다."));
 
-        if(orderState == OrderState.PAYMENT_COMPLETED)
-            returnOrdersId = orderEntity.paymentCompleted();
-
-        if(orderState == OrderState.SHIPMENT_COMPLETED)
-            returnOrdersId = orderEntity.shipmentCompleted();
-
-        return "{\"ordersId\" : "+ returnOrdersId + "}";
+        return (orderState == OrderState.PAYMENT_COMPLETED) ?
+                "{\"ordersId\" : " + orderEntity.paymentCompleted() + "}" : "{\"ordersId\" : " + orderEntity.shipmentCompleted() + "}";
     }
 }
