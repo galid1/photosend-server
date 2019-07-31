d3e9d0266d5cb822b36663276e72a130e01e9bd9
전준엽 <galid9619@gmail.com>
2019-07-31 16:11:09 +0900

    develop - test code jenkins 정상작동 test

---
+++ src/test/java/com/photosend/photosendserver01/domains/UserEntityTest.java
@@ -0,0 +1,34 @@
+package com.photosend.photosendserver01.domains;
+
+import com.photosend.photosendserver01.domains.user.domain.Token;
+import com.photosend.photosendserver01.domains.user.domain.UserEntity;
+import com.photosend.photosendserver01.domains.user.domain.UserInformation;
+import com.photosend.photosendserver01.domains.user.domain.exception.DepartureTimeException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class UserEntityTest {
+    @Test(expected = DepartureTimeException.class)
+    public void 출국_10시20시_사이_이외시간_가입_불가_test() {
+        UserInformation information =
+                UserInformation.builder()
+                        .departureTime(Timestamp.valueOf(LocalDateTime.now()))
+                        .passPortNum("123")
+                        .name("jjy")
+                        .build();
+
+        UserEntity userEntity
+                = UserEntity.builder()
+                .wechatUid("A")
+                .userInformation(information)
+                .token(Token.builder().jwtToken("ASD").build())
+                .build();
+    }
+}
